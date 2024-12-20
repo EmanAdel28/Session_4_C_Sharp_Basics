1c01f7cba5950ee7e6f9eb7a55e121f7f0b75eb2
AmiraAdel28 <amira.adel282001@gmail.com>
2024-12-20 09:24:05 +0200

    Loop Statements

---
+++ Session4_C#_Basics/Demo_Session4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo_Session4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Loop Statements
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+            //Console.WriteLine(4);
+            //Console.WriteLine(5);
+            //Console.WriteLine(6);
+            //Console.WriteLine(7);
+            //Console.WriteLine(8);
+            //Console.WriteLine(9);
+            //Console.WriteLine(10);
+
+            for(int i = 1 ; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+        }
+    }
+}
