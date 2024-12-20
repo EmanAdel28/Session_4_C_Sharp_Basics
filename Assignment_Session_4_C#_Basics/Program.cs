a225c22c011c675282ae58d2b25ba37ef85343a0
AmiraAdel28 <amira.adel282001@gmail.com>
2024-12-20 20:04:01 +0200

    1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.

--- Assignment_Session_4_C#_Basics/Program.cs
+++ Assignment_Session_4_C#_Basics/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+
+            int Number;
+            bool Flag;
+            Console.Write("Please , Enter Number ");
+            Flag = int.TryParse(Console.ReadLine(), out Number);
+
+            Console.WriteLine($"Numbers Between 1 and {Number}");
+
+            if (Flag && Number > 0) 
+            {
+                for (int i = 1; i <= Number; i++) 
+                {
+                Console.WriteLine(i);
+                }
+            }
+
+            #endregion
         }
     }
 }
