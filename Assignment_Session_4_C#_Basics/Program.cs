cacf862172b4f06c565a3aec3b97c02ec4fe3aa0
AmiraAdel28 <amira.adel282001@gmail.com>
2024-12-20 09:10:04 +0200

    Add project files.

---
+++ Assignment_Session_4_C#_Basics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_Session_4_C__Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
